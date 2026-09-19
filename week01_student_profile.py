7d2ca4304ff32418572a3d7b3fbd9c35e14c2a5b
nilaycorap <nilaycorap@gmail.com>
2026-09-19 14:48:51 +0300

    Add user input for student profile details

---
+++ week01_student_profile.py
@@ -0,0 +1,8 @@
+# Get user information
+name = input("Enter your name: ")
+department = input("Enter your department: ")
+age = input("Enter your age: ")
+career_goal = input("Enter your career goal: ")
+
+# Print student profile in required format
+print(f"--- Student Profile --- Name: {name} Department: {department} Age: {age} Career Goal: {career_goal}")
